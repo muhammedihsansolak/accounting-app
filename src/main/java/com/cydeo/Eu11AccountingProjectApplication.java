1c39171f9d670280fffb0451bf8dbefb3e4049dc
cy-ali <92939861+cy-ali@users.noreply.github.com>
2024-01-08 00:58:49 +0100

    Project started

---
+++ src/main/java/com/cydeo/Eu11AccountingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eu11AccountingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eu11AccountingProjectApplication.class, args);
+    }
+
+}
