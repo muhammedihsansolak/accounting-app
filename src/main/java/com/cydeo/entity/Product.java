9ca8b0852d9a856a374f516e005f209c19f38199
efecagrisen <efecagrisen@yahoo.com>
2024-01-09 09:43:22 +0300

    EUAC1-10_Product entity class added

---
+++ src/main/java/com/cydeo/entity/Product.java
@@ -0,0 +1,37 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.ProductUnit;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+@Where(clause = "is_deleted_false")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int quantityInStock;
+    private int lowLimitAlert;
+
+    @Enumerated(EnumType.STRING)
+    private ProductUnit productUnit;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category")
+    private Category category;
+
+
+
+
+}
