5a7fcbcaca7d630a5efdcf68ae6cd78ab58464b6
ELİF DENK <elifdenkk@gmail.com>
2024-01-09 15:29:48 +0300

    InvoiceProductDTO class created.

---
+++ src/main/java/com/cydeo/dto/InvoiceProductDTO.java
@@ -0,0 +1,33 @@
+package com.cydeo.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class InvoiceProductDTO {
+
+    private Long id;
+
+    private Integer quantity;
+
+    private BigDecimal price;
+
+    private Integer tax;
+
+    private BigDecimal total;
+
+    private BigDecimal profitLoss;
+
+    private Integer remainingQuantity;
+
+    private InvoiceDto invoice;
+
+    private ProductDto product;
+
+
+}
