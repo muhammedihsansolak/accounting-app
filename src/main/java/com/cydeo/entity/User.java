b9f83c8c8eb6981a12e4752cd0108d9cb204e4a1
Osman MERT <osman_mrt@yahoo.com>
2024-01-09 12:48:08 +0300

    EUAC1-7 - User entity and UserDTO classes is created

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,31 @@
+package com.cydeo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role; // many-to-one / will be seen under "role_id" column on the "users" table
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company; // many-to-one / will be seen under "company_id" column on the "users" table
+
+}
