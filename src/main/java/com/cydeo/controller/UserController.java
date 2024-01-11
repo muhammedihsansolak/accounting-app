4c2a72ab7f5f08c8384ea57896d08bdabea4b872
Osman MERT <osman_mrt@yahoo.com>
2024-01-11 12:31:07 +0300

    UserController commit

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -27,7 +27,7 @@ public class UserController {
     //    End-user should be able to List (display) all Users in the user_list page...
     @GetMapping("/list")
     public String listUser(Model model) {
-        List<UserDTO> userDTOList = userService.listAllUsers(); // Assuming listAllUsers retrieves all users
+        List<UserDTO> userDTOList = userService.listAllUsers();
         model.addAttribute("users", userDTOList);
         return "/user/user-list";
 
@@ -37,7 +37,6 @@ public class UserController {
     //    page and the edit form should be populated with the information of that very same User.
     @GetMapping("/edit/{id}")
     public String editUser(@PathVariable("id") Long id, Model model) {
-        // Retrieve user with the given id and populate the edit form
         UserDTO user = userService.findUserById(id);
         model.addAttribute("users", user);
         return "/user/user-update";
@@ -45,7 +44,6 @@ public class UserController {
 
     @PostMapping("/update/{id}")
     public String updateUser(@PathVariable("id") Long id, @ModelAttribute("user") UserDTO userDtoToUpdate) {
-        // Update the user in the database
         UserDTO userDTO = userService.findUserById(id);
         userService.update(userDTO, userDtoToUpdate);
 
@@ -55,17 +53,21 @@ public class UserController {
     //    End-user should be able to Delete each User (soft delete), then end up to the user_list page with updated User list.
     @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
-        // Soft delete the user with the given id
         userService.deleteUser(id);
         return "redirect:/user/user-list";
     }
 
     //    When End-User clicks on "Create-User" button, user_create page should be displayed with an Empty user form,
     @PostMapping("/create")
-    public String createUser(@RequestBody UserDTO userDTO,Model model) {
+    public String createUser(@ModelAttribute UserDTO userDTO) {
         userService.save(userDTO);
-        model.addAttribute("newUser", new User());
-        return "/user/user-create";
+        return "redirect:/user/user-list";
+    }
+
+    @GetMapping("/create")
+    public String createUserForm(Model model) {
+        model.addAttribute("newUser", new UserDTO());
+        return "user/user-create";
     }
 
 
