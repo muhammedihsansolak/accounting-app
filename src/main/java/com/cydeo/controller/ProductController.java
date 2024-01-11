741f0434c35656a484ba86ccecd70cec1b1d5adf
efecagrisen <efecagrisen@yahoo.com>
2024-01-11 12:57:21 +0300

    EUAC1-controller methods

--- src/main/java/com/cydeo/controller/ProductController.java
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -11,26 +11,38 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/product")
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
 
+    //End-user should be able to List (display) all products in the product_list page...
     @GetMapping("/list")
     public String listAllProducts(Model model){
         List<ProductDTO> productDTOList = productService.listAllProducts();
+        model.addAttribute("products", productDTOList);
 
-        return "product/product-list";
+        return "/product/product-list";
     }
 
-    @PutMapping("/update/{id}")
-    public String updateProduct(@PathVariable("id") Long id, Model model){
+    //End-user should be able to Edit each product, when click on Edit button, end-user should land on product_update page and the edit form should be populated with the information of that very same product.
+    @GetMapping("/update/{id}")
+    public String getProductToBeUpdated(@PathVariable("id") Long id, Model model){
         ProductDTO productDTOToBeUpdated = productService.findById(id);
-        productService.update();
+        model.addAttribute("product",productDTOToBeUpdated);
 
         return "product/product-update";
     }
 
+    @PutMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute("product") ProductDTO updatedProduct) {
+        productService.update(updatedProduct);
+
+        return
+    }
+
+
+    //When End-User clicks on "Create-Product" button, product_create page should be displayed with an Empty product form,
     @GetMapping("/create")
     public String createProduct(){
 
@@ -38,12 +50,13 @@ public class ProductController {
         return "/product/product-create";
     }
 
+    //End-user should be able to Delete each product(soft delete), then end up to the product_list page with updated product list.
     @DeleteMapping("/{productId}")
     public String deleteProduct(@PathVariable("productId") Long id, Model model){
         ProductDTO productDTOToBeDeleted = productService.findById(id);
         productService.delete(id);
 
-        return "/product/product-create";
+        return "redirect:/product-create";
     }
 
 
