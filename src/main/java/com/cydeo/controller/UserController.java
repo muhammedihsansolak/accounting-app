b52c44e0e2836c27f8bc225220c3ab1d44ba15f4
Osman MERT <osman_mrt@yahoo.com>
2024-01-11 19:30:20 +0300

    UserController correction

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
 
     private final RoleService roleService;
