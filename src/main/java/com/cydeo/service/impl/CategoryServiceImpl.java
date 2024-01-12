b7e62069d98dd6206bc661baeea30a98793b7854
aysegul <aysegulkafaliyilmaz@gmail.com>
2024-01-13 00:39:42 +0300

    EUAC1-23

---
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,67 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CategoryDTO;
+import com.cydeo.entity.Category;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.CategoryRepository;
+import com.cydeo.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+
+    @Override
+    public CategoryDTO findById(Long id) {
+        Optional<Category> category = categoryRepository.findById(id);
+        return mapperUtil.convert(category, new CategoryDTO());
+    }
+
+    @Override
+    public List<CategoryDTO> findAll() {
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream().map(category -> mapperUtil.convert(category, new CategoryDTO())).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<CategoryDTO> listAllCategories() {
+
+        List<Category> categoryList = categoryRepository.findAll();
+        return categoryList.stream().map(category -> mapperUtil.convert(category, new CategoryDTO())).
+                collect(Collectors.toList());
+    }
+
+
+    @Override
+    public CategoryDTO save(CategoryDTO dto) {
+        Category category = mapperUtil.convert(dto, new Category());
+        categoryRepository.save(category);
+        return mapperUtil.convert(category, new CategoryDTO());
+
+
+    }
+
+
+    @Override
+    public CategoryDTO update(CategoryDTO dto, Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow();
+        Category convertedCategory = mapperUtil.convert(dto, new Category());
+        categoryRepository.save(convertedCategory);
+        return mapperUtil.convert(convertedCategory, new CategoryDTO());
+
+    }
+
+
+}
