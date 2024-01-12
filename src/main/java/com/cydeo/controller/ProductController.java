659c8d501cad4144811c6c903342f67c094681f4
efecagrisen <efecagrisen@yahoo.com>
2024-01-12 17:17:19 +0300

    EUAC1-controller methods completed

--- src/main/java/com/cydeo/controller/ProductController.java
+++ src/main/java/com/cydeo/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProductDTO;
+import com.cydeo.enums.ProductUnit;
 import com.cydeo.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,7 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
-    //private final CategoryService categoryService;
+    private final CategoryService categoryService;
 
 
     //End-user should be able to List (display) all products in the product_list page...
@@ -28,21 +29,21 @@ public class ProductController {
     }
 
     //End-user should be able to Edit each product, when click on Edit button, end-user should land on product_update page and the edit form should be populated with the information of that very same product.
-//    @GetMapping("/update/{id}")
-//    public String editProduct(@PathVariable("id") Long id, Model model){
-//        ProductDTO productToBeUpdated = productService.findById(id);
-//        model.addAttribute("products",productToBeUpdated);
-//        model.addAttribute("categories", productService.getAllCategories());
-//        model.addAttribute("productUnits", productService.getAllProductUnits());
-//        return "/product/product-update";
-//    }
-//
-//    @PostMapping("/update/{id}")
-//    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute("product") ProductDTO productDtoToBeUpdated) {
-//        productService.update(id, productDtoToBeUpdated);
-//
-//        return "redirect:/product/product-list";
-//    }
+    @GetMapping("/update/{id}")
+    public String editProduct(@PathVariable("id") Long id, Model model){
+        ProductDTO productToBeUpdated = productService.findById(id);
+        model.addAttribute("products",productToBeUpdated);
+        model.addAttribute("categories", productService.getAllCategories());
+        model.addAttribute("productUnits", ProductUnit.values());
+        return "/product/product-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") Long id, @ModelAttribute("product") ProductDTO productDtoToBeUpdated) {
+        productService.update(id, productDtoToBeUpdated);
+
+        return "redirect:/product/product-list";
+    }
 
 
     //When End-User clicks on "Create-Product" button, product_create page should be displayed with an Empty product form,
