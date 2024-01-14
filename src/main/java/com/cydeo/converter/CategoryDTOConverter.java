27c95f7b87ffba0b2cd93513412954f242568aef
aysegul <aysegulkafaliyilmaz@gmail.com>
2024-01-14 15:52:48 +0300

    EUAC1-40 CategoryDTOConverter fixed.

--- src/main/java/com/cydeo/converter/CategoryDTOConverter.java
+++ src/main/java/com/cydeo/converter/CategoryDTOConverter.java
@@ -4,16 +4,20 @@ import com.cydeo.dto.CategoryDTO;
 import com.cydeo.entity.Category;
 import com.cydeo.service.CategoryService;
 import org.springframework.stereotype.Component;
+import org.springframework.core.convert.converter.Converter;
+
 
 @Component
-public class CategoryDTOConverter {
+public class CategoryDTOConverter implements Converter <Long, CategoryDTO> {
 
     CategoryService categoryService;
 
     public CategoryDTOConverter(CategoryService categoryService) {
+
         this.categoryService = categoryService;
     }
 
+    @Override
     public CategoryDTO convert(Long id) {
         if (id==null || id.equals("")){
             return null;
