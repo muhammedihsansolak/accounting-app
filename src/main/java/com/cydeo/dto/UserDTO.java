91487dd5ecfe94b24ddff5a6ac46abdaae18e813
Osman MERT <osman_mrt@yahoo.com>
2024-01-15 14:56:27 +0300

    Validation part added

--- src/main/java/com/cydeo/dto/UserDTO.java
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -1,31 +1,63 @@
 package com.cydeo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import javax.validation.constraints.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDTO {
 
     private Long id;
+
+    @NotBlank(message = "Email is required field.")
+    @NotNull
+    @Email(message = "A user with this email already exists. Please try with different email.")
     private String username;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank
+    @Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String password;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
     private String confirmPassword;
+
+    @NotBlank(message = "First Name is required field.")
+    @Size(min = 2, max = 50, message = "First Name must be between 2 and 50 characters long.")
     private String firstname;
+
+    @NotBlank(message = "Last Name is required field.")
+    @Size(min = 2, max = 50, message = "First Name must be between 2 and 50 characters long.")
     private String lastname;
+
+    @Pattern(regexp = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+            + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$")
     private String phone;
+
+    @NotNull
     private RoleDTO role;
+    @NotNull
     private CompanyDTO company;
     private boolean enabled;
 
     private boolean isOnlyAdmin; //(should be true if this user is only admin of any company.) I will write in business logic part
 
+    public void setPassword(String password) {
+        this.password = password;
+        checkConfirmPassword();
+    }
+
+    private void checkConfirmPassword() {
+        if (this.password == null || this.confirmPassword == null) {
+            return;
+        } else if (!this.password.equals(this.confirmPassword)) {
+            this.confirmPassword = null;
+        }
+    }
 }
