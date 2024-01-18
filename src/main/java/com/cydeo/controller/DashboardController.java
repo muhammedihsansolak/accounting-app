cdb1237b15864b64d0319bb68e82bcb23d875165
ELİF DENK <elifdenkk@gmail.com>
2024-01-18 17:03:10 +0300

    some methods added

--- src/main/java/com/cydeo/controller/DashboardController.java
+++ src/main/java/com/cydeo/controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.cydeo.controller;
 
+import com.cydeo.client.ExchangeClient;
+import com.cydeo.dto.InvoiceDTO;
 import com.cydeo.service.DashboardService;
 import com.cydeo.service.InvoiceService;
 import org.springframework.stereotype.Controller;
@@ -7,26 +9,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/dashboard")
 public class DashboardController {
 
    private final InvoiceService invoiceService;
    private final DashboardService dashboardService;
+   private final ExchangeClient exchangeClient;
 
-    public DashboardController(InvoiceService invoiceService, DashboardService dashboardService) {
+
+    public DashboardController(InvoiceService invoiceService, DashboardService dashboardService, ExchangeClient exchangeClient) {
         this.invoiceService = invoiceService;
         this.dashboardService = dashboardService;
+        this.exchangeClient = exchangeClient;
     }
 
 
     @GetMapping
     public String openDashboardPage(Model model){
 
+
         model.addAttribute("invoices",invoiceService.findTop3ByCompanyOrderByDateDesc());
-        model.addAttribute("summaryNumbers",dashboardService.totalCost());
-        model.addAttribute("summaryNumbers",dashboardService.totalSales());
-        model.addAttribute("summaryNumbers",dashboardService.profitLoss());
+        model.addAttribute("summaryNumbers",dashboardService.summaryNumbersCalculation() );
+        model.addAttribute("exchangeRates",exchangeClient.getExchangesRates().getUsd());
 
         return "/dashboard";
     }
