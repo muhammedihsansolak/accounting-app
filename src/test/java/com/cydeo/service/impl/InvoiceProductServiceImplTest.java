2880d01b5aa3afb99cb5d00be8178ae00b383302
Muhammed <muhammedihsansolak@gmail.com>
2024-01-24 09:52:19 +0300

    doesProductHaveEnoughStock() removeInvoiceProductFromInvoice() unit tests DONE

--- src/test/java/com/cydeo/service/impl/InvoiceProductServiceImplTest.java
+++ src/test/java/com/cydeo/service/impl/InvoiceProductServiceImplTest.java
@@ -2,7 +2,9 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.InvoiceDTO;
 import com.cydeo.dto.InvoiceProductDTO;
+import com.cydeo.dto.ProductDTO;
 import com.cydeo.entity.InvoiceProduct;
+import com.cydeo.entity.Product;
 import com.cydeo.exception.InvoiceProductNotFoundException;
 import com.cydeo.mapper.MapperUtil;
 import com.cydeo.repository.InvoiceProductRepository;
@@ -12,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -39,10 +43,10 @@ class InvoiceProductServiceImplTest {
     private InvoiceProductServiceImpl invoiceProductService;
 
     /*
-    ************** findById() **************
+     ************** findById() **************
      */
     @Test
-    void should_throw_exception_when_invoice_product_not_found_with_id(){
+    void should_throw_exception_when_invoice_product_not_found_with_id() {
         //given
         Long id = 1L;
 
@@ -56,7 +60,7 @@ class InvoiceProductServiceImplTest {
     }
 
     @Test
-    void should_execute_mapper_at_least_once_and_should_return_what_is_converted(){
+    void should_execute_mapper_at_least_once_and_should_return_what_is_converted() {
         //given
         Long id = 1L;
         InvoiceProduct invoiceProduct = new InvoiceProduct();
@@ -116,14 +120,15 @@ class InvoiceProductServiceImplTest {
 
         assertEquals(2, result.size()); // Check the size of the result list
         // Assuming that the total amount is calculated and set in the DTO
-         assertEquals(BigDecimal.valueOf(220), invoiceProductDTO1.getTotal()); // Total = 100 * 2 + 20
-         assertEquals(BigDecimal.valueOf(240), invoiceProductDTO2.getTotal()); // Total = 200 * 1 + 40
+        assertEquals(BigDecimal.valueOf(220), invoiceProductDTO1.getTotal()); // Total = 100 * 2 + 20
+        assertEquals(BigDecimal.valueOf(240), invoiceProductDTO2.getTotal()); // Total = 200 * 1 + 40
     }
+
     /*
      ************** findByInvoiceId() **************
      */
     @Test
-    void should_throw_exception_when_invoice_product_can_not_found_with_id(){
+    void should_throw_exception_when_invoice_product_can_not_found_with_id() {
         //given
         Long id = 1L;
 
@@ -140,7 +145,7 @@ class InvoiceProductServiceImplTest {
      ************** deleteById() **************
      */
     @Test
-    void should_sets_is_deleted_field_to_true(){
+    void should_sets_is_deleted_field_to_true() {
         //given
         Long id = 1L;
 
@@ -164,6 +169,119 @@ class InvoiceProductServiceImplTest {
         verify(repository).save(invoiceToDelete);
         verify(mapper).convert(deleted, new InvoiceProductDTO());
         assertNotNull(result);
+    }
+
+    /*
+     ************** removeInvoiceProductFromInvoice() **************
+     */
+    //TODO
+    @Test
+    void should_remove_invoice_product_from_invoice() {
+        // Given
+        Long invoiceId = 1L;
+        Long invoiceProductId = 2L;
+
+        InvoiceProduct invoiceProduct1 = new InvoiceProduct();
+        InvoiceProduct invoiceProduct2 = new InvoiceProduct();
+
+        List<InvoiceProduct> invoiceProductList = new ArrayList<>();
+        invoiceProductList.add(invoiceProduct1);
+        invoiceProductList.add(invoiceProduct2);
+
+        InvoiceProductDTO invoiceProductDTO1 = new InvoiceProductDTO();
+        invoiceProductDTO1.setId(2L); // This product should be deleted
+        invoiceProductDTO1.setPrice(BigDecimal.valueOf(50));
+        invoiceProductDTO1.setTax(10);
+        invoiceProductDTO1.setQuantity(1);
+
+        InvoiceProductDTO invoiceProductDTO2 = new InvoiceProductDTO();
+        invoiceProductDTO2.setId(3L); // This product should NOT be deleted
+        invoiceProductDTO2.setPrice(BigDecimal.valueOf(100));
+        invoiceProductDTO2.setTax(5);
+        invoiceProductDTO2.setQuantity(1);
+
+        List<InvoiceProductDTO> invoiceProductDTOList = new ArrayList<>();
+        invoiceProductDTOList.add(invoiceProductDTO1);
+        invoiceProductDTOList.add(invoiceProductDTO2);
+
+        when(repository.findByInvoiceId(invoiceId)).thenReturn(invoiceProductList);
+        when(mapper.convert(invoiceProduct1, new InvoiceProductDTO())).thenReturn(invoiceProductDTO1);
+        when(mapper.convert(invoiceProduct2, new InvoiceProductDTO())).thenReturn(invoiceProductDTO2);
+        when(invoiceService.calculateTaxForProduct(invoiceProductDTO1)).thenReturn(BigDecimal.valueOf(5));
+        when(invoiceService.calculateTaxForProduct(invoiceProductDTO2)).thenReturn(BigDecimal.valueOf(5));
+        when(invoiceProductService.findByInvoiceId(invoiceId)).thenReturn(invoiceProductDTOList);
+
+        // When
+        invoiceProductService.removeInvoiceProductFromInvoice(invoiceId, invoiceProductId);
+
+        // Then
+        verify(invoiceProductService).findByInvoiceId(invoiceId);
+        verify(invoiceProductService).deleteById(invoiceProductId);
+        verify(invoiceProductService, never()).deleteById(3L); // Verifying that it's not called for invoiceProductDTO2
+    }
+
+    /*
+     ************** create() **************
+     */
+    @Test
+    void should_create_invoice_product(){
+        //given
+        Long invoiceId = 1L;
+
+        InvoiceDTO invoiceDTO = new InvoiceDTO();
+        InvoiceProduct invoiceProduct = new InvoiceProduct();
+        InvoiceProductDTO invoiceProductDTO = new InvoiceProductDTO();
+        InvoiceProduct savedInvoiceProduct = new InvoiceProduct();
+        InvoiceProductDTO savedInvoiceProductDTO = new InvoiceProductDTO();
+
+        when(invoiceService.findById(invoiceId)).thenReturn(invoiceDTO);
+        when(mapper.convert(eq(invoiceProductDTO), any(InvoiceProduct.class))).thenReturn(invoiceProduct);
+        when(repository.save(invoiceProduct)).thenReturn(savedInvoiceProduct);
+        when(mapper.convert(eq(savedInvoiceProduct),  any(InvoiceProductDTO.class))).thenReturn(savedInvoiceProductDTO);
+
+        // When
+        invoiceProductService.create(invoiceProductDTO, invoiceId);
+
+        // Then
+        verify(invoiceService).findById(invoiceId);
+        verify(mapper).convert(eq(invoiceProductDTO), any(InvoiceProduct.class));
+        verify(repository).save(invoiceProduct);
+        verify(mapper).convert(eq(savedInvoiceProduct), any(InvoiceProductDTO.class));
+    }
+
+    /*
+     ************** doesProductHaveEnoughStock() **************
+     */
+    @Test
+    void product_has_enough_stock(){
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setQuantityInStock(15);
+
+        InvoiceProductDTO invoiceProductDTO = new InvoiceProductDTO();
+        invoiceProductDTO.setProduct(productDTO);
+        invoiceProductDTO.setQuantity(10);//less than product stock
+
+        BindingResult bindingResult = mock(BindingResult.class);
+
+        BindingResult result = invoiceProductService.doesProductHaveEnoughStock(invoiceProductDTO, bindingResult);
+
+        verify(bindingResult, never()).addError(any(ObjectError.class)); //if product has enough stock we DO NOT add error to bindingResult
+    }
+
+    @Test
+    void product_do_not_have_enough_stock(){
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setQuantityInStock(15);
+
+        InvoiceProductDTO invoiceProductDTO = new InvoiceProductDTO();
+        invoiceProductDTO.setProduct(productDTO);
+        invoiceProductDTO.setQuantity(100);//more than product stock
+
+        BindingResult bindingResult = mock(BindingResult.class);
+
+        BindingResult result = invoiceProductService.doesProductHaveEnoughStock(invoiceProductDTO, bindingResult);
+
+        verify(bindingResult, atLeastOnce()).addError(any(ObjectError.class)); //if product has enough stock we add error to bindingResult
     }
 
 }
